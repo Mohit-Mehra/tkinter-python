28a6592aac5bf2d225c50a8e19bb108288057630
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-07-09 22:44:50 +0530

    more in grid

--- 19_Grid.py
+++ 19_Grid.py
@@ -15,12 +15,16 @@ button2 = ttk.Button(window, text = 'Button 2')
 entry = ttk.Entry(window)
 
 # Define a grid
-window.columnconfigure(0, weight = 1)
-window.columnconfigure(1, weight = 1)
+window.columnconfigure((0,1,2), weight = 1)
+window.columnconfigure(3, weight = 10)
 window.rowconfigure(0, weight = 1)
+window.rowconfigure(1, weight = 1)
+window.rowconfigure(2, weight = 1)
+window.rowconfigure(3, weight = 3)
 
 # Pace Widgets
-label1.grid(row = 0, column = 0, sticky = 'w')
-label2.grid(row = 0, column = 1,sticky = 'e')
+label1.grid(row = 0, column = 0, sticky = 'nsew')
+label2.grid(row = 0, column = 1,sticky = 'w')
+label3.grid(row = 0, column = 3,sticky = 'ew')
 
 window.mainloop()
